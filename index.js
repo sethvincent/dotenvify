f35fe97f93330391820d38606756eaf91b069956
sethvincent <sethvincent@gmail.com>
2019-10-28 16:44:02 -0700

    fix transform

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const dotenv = require('dotenv')
 const envify = require('envify/custom')
 
-module.exports = function dotenvify (env, opts) {
+module.exports = function dotenvify (file, opts) {
   const { error } = dotenv.config(opts)
   if (error) throw error
-  return envify(env)
+  const transform = envify(process.env)
+  return transform(file, opts)
 }
